b76cefe76f37d89bc865d9e1c094f18e6899c6cf
traaitt | TRRXITTE inc <traaitt@icloud.com>
2021-03-21 19:15:52 +0100

    add updated Information

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -353,7 +353,7 @@ namespace CryptoNote
     const uint32_t DATABASE_DEFAULT_MAX_OPEN_FILES = 125; // 125 files
     const uint16_t DATABASE_DEFAULT_BACKGROUND_THREADS_COUNT = 4; // 4 DB threads
 
-    const char LATEST_VERSION_URL[] = "http://latest.traaittPlatform.com";
+    const char LATEST_VERSION_URL[] = "http://github.com/TRRXITTE/traaittPlatform/releases";
 
     const std::string LICENSE_URL = "https://github.com/TRRXITTE/traaittPlatform/blob/master/LICENSE";
 
